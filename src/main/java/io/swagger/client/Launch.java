542aad9cc99e2a1b1bd707233a4c6aaffcd42c2f
jhgreen19 <jhgreen19@gmail.com>
2019-04-29 20:03:25 -0700

    cleaning up extra code used during debugging to make it easier to read

--- src/main/java/io/swagger/client/Launch.java
+++ src/main/java/io/swagger/client/Launch.java
@@ -8,24 +8,20 @@ import static java.lang.System.out;
 
 public class Launch {
 
-	private final static String basePath = "https://api-sandbox.dwolla.com";	
-	//private final static String basePath = "https://api-sandbox.dwolla.com/customers";
+	private final static String basePath = "https://api-sandbox.dwolla.com";
 	
 	//for now, get this from below URL and change EVERY 60 Minutes!
 	//https://dashboard-sandbox.dwolla.com/applications-legacy
 	private final static String accessToken = "Lr8WWn9YtCO82iUKHNBCokSZ9tfZzTc8xcP3V1mL3KdJd8n21K";
-				//last used 2:58pm PDT, 4/25/19
+	//last used 2:58pm PDT, 4/25/19
 	
 	public static void main(String[] args) throws ApiException {
 		// TODO Auto-generated method stub 
 
 		ApiClient a = new ApiClient();
-		//a.setBasePath(basePath);
-		//a.setAccessToken(accessToken);
+		a.setBasePath(basePath);
+		a.setAccessToken(accessToken);
 
-		a.setBasePath("https://api-sandbox.dwolla.com");
-		a.setAccessToken("YEa8OpcpMLYcIhkGz6aFc0Ge6GDgR14yxZPO68knt79amUN0pK");
-		
 		CustomersApi c = new CustomersApi();
 
 		/////////////// Abstract and move into SetUpCustomer //////////////////////
@@ -37,11 +33,8 @@ public class Launch {
 
 		Unit$ r = c.create(myNewCust);
 		/*
-
-		out.println("r.getLocationHeader(): " + r.getLocationHeader());		
-		
+		out.println("r.getLocationHeader(): " + r.getLocationHeader());				
 		/////////////// Abstract and move into SetUpCustomer //////////////////////
-
 		out.println("Launchit!");
 		*/
 	}
